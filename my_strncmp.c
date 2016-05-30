119f47012fb3c788c1e48be3b1222dfef9c2c852
Maxime Chamot <maxime.chamot@epitech.eu>
2016-05-30 13:26:59 +0100

    libmy.h updated: epitech norm

--- my_strncmp.c
+++ my_strncmp.c
@@ -6,5 +6,9 @@ int		my_strncmp(const char *s1, const char *s2, int n)
   if (s1 != NULL && s2 != NULL && *s1 != '\0' && s2 != '\0' && *s1 == *s2 && n > 1)
     my_strncmp(s1 + 1, s2 + 1, n - 1);
   else
-    return (*s1 - *s2);
+    {
+      if (s1 != NULL && s2 != NULL)
+	return (*s1 - *s2);
+      return (0);
+    }
 }
